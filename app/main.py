f0df3ad3a7fefedb914fcfc872cbabf039682067
justinnonso05 <chinonsoali2005@gmail.com>
2025-03-18 20:15:35 +0100

    update cors

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 origins = [
     "http://127.0.0.1:5500",  # Your frontend running on this port
     "http://localhost:5500",
-    "https://bd8d-102-89-69-109.ngrok-free.app"   # Alternative localhost URL
+    "https://justinnonso05.github.io/url-insights-frontend/"  # Alternative localhost URL
 ]
 # Enable CORS middleware
 app.add_middleware(
